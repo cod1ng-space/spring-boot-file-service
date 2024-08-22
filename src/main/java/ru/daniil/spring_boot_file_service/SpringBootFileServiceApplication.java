10e64343a2ebafb1ca6c95910b03c693f87d5e3a
Daniil Voronin <danilka.voronin.05@gmail.com>
2024-08-22 22:52:24 +0300

    first commit

---
+++ src/main/java/ru/daniil/spring_boot_file_service/SpringBootFileServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.daniil.spring_boot_file_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/daniil/spring_boot_file_service/SpringBootFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.daniil.spring_boot_file_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
